8233b96f8db2c2fb59317f4c962d6a3a5f62b644
MchrDigital <ferramentas@mchrdigital.com>
2025-07-17 19:29:22 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.body.style.backgroundColor = '#111';
-document.getElementById('root').innerHTML = `
-  <h1 style="color: white; text-align: center; padding-top: 100px;">
-    Página publicada com sucesso 🚀
-  </h1>
-`;
